6049c9a6715026d4b6d476a491c7d33bb3a7ce95
Eduardo555 <eduardospillereanzolin@gmail.com>
2018-05-30 23:50:14 -0300

    Front End

---
+++ src/main/java/br/com/ordemdeev/quizzes/web/PerguntasBean.java
@@ -0,0 +1,15 @@
+package br.com.ordemdeev.quizzes.web;
+
+import br.com.ordemdeev.quizzes.usuario.Usuario;
+import br.com.ordemdeev.quizzes.usuario.UsuarioRN;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "PerguntasBean")
+@RequestScoped
+public class PerguntasBean {
+
+
+
+}
